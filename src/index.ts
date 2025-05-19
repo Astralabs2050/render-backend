e9df40eba22f8faa95b2bfa51390c085b59d44cc
Mac <lawblaze4@gmail.com>
2025-05-19 20:26:14 +0100

    made some changes

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ const allowedOrigins = [
   "http://51.20.37.218:3004",
   "http://localhost:3001",
   "http://51.20.37.218:3005/",
+  "www.astra.fashion"
   // Add more origins as needed
 ];
 
@@ -46,6 +47,7 @@ async function startServer() {
         "http://localhost:3000",
         "http://localhost:3000",
         "http://localhost:3001",
+        "www.astra.fashion",
         "http://51.20.37.218:3005/",
         // Add more origins as needed
       ], // Allow frontend port
