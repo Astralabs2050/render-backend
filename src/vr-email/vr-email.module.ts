617d0b4049d337b215c2007253c193fe2e14b4d1
Samuel <samueladelowo92@gmail.com>
2025-11-18 11:57:09 +0100

    vr updates

--- src/vr-email/vr-email.module.ts
+++ src/vr-email/vr-email.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VrEmailController } from './vr-email.controller';
 import { VrEmailService } from './vr-email.service';
 import { VrEmail } from './entities/vr-email.entity';
-import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([VrEmail, User])],
+  imports: [TypeOrmModule.forFeature([VrEmail])],
   controllers: [VrEmailController],
   providers: [VrEmailService],
   exports: [VrEmailService],
